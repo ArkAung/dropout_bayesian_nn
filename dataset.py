09eed77404580b77e719504e6ce086947c6ee061
Arkar Aung <arica.aung@gmail.com>
2020-05-12 21:18:03 +0630

    Changes in plotting, dataset, networks
    - Fix error with image resizing in plotting
    - Choose dataset type with ENUMs
    - Include batch size in dataset
    - Accept Dataset in network train method
    - Add prediction method in network
    - Add cells for plotting samples images in notebook
    - Add cells for training and getting predictions in notebook

--- dataset.py
+++ dataset.py
@@ -1,25 +1,33 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 
 class Dataset:
-    def __init__(self, path, data_shape, train: bool,
+    TRAIN = 1
+    VAL = 2
+    TEST = 3
+
+    def __init__(self, path: str, target_size: Tuple[int, int], dataset_type, batch_size: int,
                  class_filter: List[str] = None, label_mapping: Dict[int, str] = None):
         self.path = path
-        if train:
+        if dataset_type == Dataset.TRAIN:
             self.dataset_subset = 'training'
             self.dataset_shuffle = True
-        else:
+        elif dataset_type == Dataset.VAL:
             self.dataset_subset = 'validation'
             self.dataset_shuffle = False
+        elif dataset_type == Dataset.TEST:
+            self.dataset_subset = None
+            self.dataset_shuffle = False
+        self.dataset_type = dataset_type
         self.label_mapping = label_mapping
-        self.data_shape = data_shape
+        self.target_size = target_size
+        self.batch_size = batch_size
         self.class_filter = class_filter
+        self.datagenerator = self._get_datagenerator()
 
-    @staticmethod
-    def _prepare_generator(train=True):
-        if train:
+    def _prepare_generator(self):
+        if self.dataset_type == Dataset.TRAIN:
             return ImageDataGenerator(
                                 rescale=1./255,
                                 rotation_range=40,
@@ -29,17 +37,18 @@ class Dataset:
                                 zoom_range=0.2,
                                 horizontal_flip=True,
                                 fill_mode='nearest',
-                                validation_split=0.1
-            )
+                                validation_split=0.2
+                                )
         else:
             return ImageDataGenerator(rescale=1./255)
 
-    def get_datagenerator(self, train=True):
-        prepared_generator = self._prepare_generator(train)
+    def _get_datagenerator(self):
+        prepared_generator = self._prepare_generator()
         return prepared_generator.flow_from_directory(self.path,
-                                                      target_size=self.data_shape,
+                                                      target_size=self.target_size,
                                                       class_mode='categorical',
                                                       subset=self.dataset_subset,
                                                       shuffle=self.dataset_shuffle,
+                                                      batch_size=self.batch_size,
                                                       seed=1337,
                                                       classes=self.class_filter)
--- network.py
+++ network.py
@@ -1,21 +1,21 @@
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Flatten, Dense
-from tensorflow.keras.callbacks import EarlyStopping
-
 import os
-import numpy as np
 import random
-
 from typing import Tuple
+from dataset import Dataset
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.keras.models import Model
 
 
 class Network:
     def __init__(self, input_shape: Tuple[int, int, int], dropout_rate: float, num_classes: int):
         self.input_shape = input_shape
-        self.model = self._build_model()
         self.dropout_rate = dropout_rate
         self.num_classes = num_classes
+        self.model = self._build_model()
 
     @staticmethod
     def _set_seeds(seed):
@@ -51,14 +51,17 @@ class Network:
 
         return model
 
-    def train_model(self, train_gen, val_gen, batch_size: int, epochs: int) -> None:
+    def train_model(self, train_dataset: Dataset, val_dataset: Dataset, epochs: int) -> None:
         self._set_seeds(1337)
 
         es = EarlyStopping(monitor='val_accuracy', mode='auto', restore_best_weights=True, verbose=1, patience=7)
 
-        train_steps = train_gen.samples / batch_size
-        val_steps = val_gen.samples / batch_size
+        train_steps = train_dataset.datagenerator.samples / train_dataset.datagenerator.batch_size
+        val_steps = val_dataset.datagenerator.samples / val_dataset.datagenerator.batch_size
 
-        self.model.fit(train_gen, steps_per_epoch=train_steps, epochs=epochs,
-                       validation_data=val_gen, validation_steps=val_steps,
-                       callbacks=[es], verbose=2)
+        self.model.fit(train_dataset.datagenerator, steps_per_epoch=train_steps, epochs=epochs,
+                       validation_data=val_dataset.datagenerator, validation_steps=val_steps,
+                       callbacks=[es], verbose=1)
+
+    def get_predictions(self, test_dataset: Dataset) -> np.ndarray:
+        return self.model.predict(test_dataset)
